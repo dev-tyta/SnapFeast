1ea9f0b64a20c286734485e82edaaa0f14fa4f6f
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-05-25 21:59:56 +0100

    Creating a class for facial processing

--- utils/face_extraction.py
+++ utils/face_extraction.py
@@ -6,9 +6,29 @@ from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
 import matplotlib.pyplot as plt
 
-def face_extract(img_path, required_size=(224,224)):
-    pixel = plt.imread(img_path)
-    mtcnn_detector = MTCNN()
-    faces = mtcnn_detector.detect_faces(pixel)
 
-    face_array
+class FacialProcessing():
+    def __init__(self, img_path, required_size=(224,224)):
+        self.face_array = []
+        self.image_path = img_path
+        self.img_size = required_size
+
+
+    def face_extract(self):
+        pixel = plt.imread(self.image_path)
+        mtcnn_detector = MTCNN()
+        faces = mtcnn_detector.detect_faces(pixel)
+
+        for i in range(len(faces)):
+            x1, y1, width, height = faces[i]["box"]
+            x2, y2 = x1+width, y1+height
+            face = pixel[y1:y2, x1:x2]
+            image = Image.fromarray(face)
+            image = image.resize(self.img_size)
+            self.face_array.append(np.asarray(image))
+
+        return self.face_array
+
+
+    def extract_embeddings(self):
+        pass
