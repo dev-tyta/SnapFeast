70f4fd85c032a3007f70b0c187caba6a94d0dd7a
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-06-03 16:29:46 +0100

    Creating script for facial recognition system

--- utils/facial_processing.py
+++ utils/facial_processing.py
@@ -5,30 +5,58 @@ from mtcnn.mtcnn import MTCNN
 from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
 import matplotlib.pyplot as plt
+from PIL import Image
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 class FacialProcessing():
     def __init__(self, img_path, required_size=(224,224)):
-        self.face_array = []
+        self.face_array = None
         self.image_path = img_path
         self.img_size = required_size
+        self.model = VGGFace(model="resnet50", include_top=False,
+                             pooling="avg")
 
 
     def face_extract(self):
-        pixel = plt.imread(self.image_path)
+        self.img = Image.open(self.image_path)
+        self.img = self.img.convert("RGB")
+        pixel = (self.img)
+        
         mtcnn_detector = MTCNN()
         faces = mtcnn_detector.detect_faces(pixel)
 
-        for i in range(len(faces)):
-            x1, y1, width, height = faces[i]["box"]
+        if faces:
+            num_face = len(faces)
+
+            print(f"Found {num_face} face(s) in the image")
+
+            x1, y1, width, height = faces[0]["box"]
             x2, y2 = x1+width, y1+height
             face = pixel[y1:y2, x1:x2]
             image = Image.fromarray(face)
             image = image.resize(self.img_size)
-            self.face_array.append(np.asarray(image))
+            self.face_array = np.asarray(image)
 
-        return self.face_array
+            return self.face_array
 
+        else:
+            return None
 
     def extract_embeddings(self):
-        pass
+        if self.face_array is not None:
+            face = self.face_array.astype("float32")
+            face = np.expand_dims(face, axis=0)
+            face = preprocess_input(face, version=2)
+            embeddings = self.model.predict(face)
+            embeddings = np.squeeze(embeddings, axis=0)
+
+            return embeddings
+        
+        else: 
+            return None
+        
+    def match_faces(self, new_embeddings, saved_embeddings):
+        max_similarity = 0
+        for filename, stored_embeddings in saved_embeddings.items():
+            similarity = cosine_similarity(new_embeddings.reshape()
