50fb3690d9ba6d831b0a6f62d661bd8397ca029a
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-06-20 00:29:29 +0100

    Making new changes

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,14 +1,24 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from typing import Any
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 
 from .models import Customer
 
 
-class CustomerCreationForm(UserCreationForm):
+class UserSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Customer
-        fields = UserCreationForm.Meta.fields + ('name', 'email', 'phone', 'profile_pic')   
+        fields = UserCreationForm.Meta.fields + ('name', 'email', 'age', 'profile_pic', 'preferences')   
 
-class CustomerChangeForm(UserChangeForm):
+class UserUpdateForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = Customer
-        fields = UserChangeForm.Meta.fields
+        fields = UserChangeForm.Meta.fields + ('name', 'email', 'age', 'profile_pic', 'preferences')
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__( *args, **kwargs)
+        self.fields['username'].label = 'Email'
+        self.fields['password'].label = 'Password'
