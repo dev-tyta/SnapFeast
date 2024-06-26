775454642ac936145f059f5f2e5e7f0da117c58b
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-06-26 17:29:24 +0100

    Let's see

--- src/snapfeast/settings.py
+++ src/snapfeast/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
-    "users.apps.UserConfig",
-    "orders.apps.OrdersConfig"
+    "rest_framework",  
+    "users",
+    "orders"
 ]
 
 MIDDLEWARE = [
--- src/users/models.py
+++ src/users/models.py
@@ -20,6 +20,7 @@ cloudinary.config(
 
 # Create your models here.
 class UserProfile(AbstractUser):
+    app_label = "user"
     first_name = models.CharField(max_length=200, blank=False)
     last_name = models.CharField(max_length=200, blank=False)
     mail = models.EmailField(max_length=200, unique=True)
