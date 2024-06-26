f0652455b8aa08b2c3ef8bfa4166652ecbce2319
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-06-26 10:42:11 +0100

    Finishing off views for orders part

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,16 +1,30 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .models import MealOrder
 from .serializers import MealOrderSerializer
 
 
 # Create your views here.
-class OrderList(generics.ListCreateAPIView):
+class CreateOrderView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = MealOrder.objects.all()
     serializer_class = MealOrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user_id = self.request.user)
+
+
+class OrderList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = MealOrder.objects.all()
+    serializer_class = MealOrderSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user_id=self.request.user)
 
 
 class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = MealOrder.objects.all()
     serializer_class = MealOrderSerializer
-
