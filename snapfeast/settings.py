905e12d38ad84c41836cc50b1b1677f84a2fff5b
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-06-29 20:31:44 +0100

    Superuser Created

--- snapfeast/settings.py
+++ snapfeast/settings.py
@@ -110,20 +110,20 @@ SPECTACULAR_SETTINGS = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "snapfeast_db",
-        "USER": "postgres",
-        "PASSWORD": "testimony",
-        "HOST": 'localhost',
-        "PORT": '5432',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "snapfeast_db",
+#         "USER": "postgres",
+#         "PASSWORD": "testimony",
+#         "HOST": 'localhost',
+#         "PORT": '5432',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 
 
